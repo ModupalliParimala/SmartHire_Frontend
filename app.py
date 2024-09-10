7d3534876078171941a1759ed61b8059ac45c195
Parimala Modupalli <parimala.modupalli@msg-global.com>
2024-09-10 13:00:41 +0530

    Added app server

--- app.py
+++ app.py
@@ -12,6 +12,8 @@ app = dash.Dash(__name__,
                 external_stylesheets=[dbc.themes.BOOTSTRAP, 
                                       "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"], 
                 suppress_callback_exceptions=True)
+server = app.server 
+
 # Define the app layout
 
 app.title = 'Smart Hire' 
